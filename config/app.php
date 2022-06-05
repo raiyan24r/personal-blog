37ee9333cfccb4732b1e07efa69e5309656c998c
raiyan24r <raiyan24r@gmail.com>
2022-06-06 02:42:39 +0600

    changed timezone to dhaka

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => env('APPLICATION_NAME') ?: 'Raiyan',
+    'name' => env('APPLICATION_NAME') ?: 'Raiyan Blog',
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://pigjian.app'),
+    'url' => env('APP_URL', 'https://writer.raiyanibnehafiz.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
@@ -66,7 +66,7 @@ return [
     |
     */
 
-    'timezone' => env('TIMEZONE') ?: 'UTC',
+    'timezone' => 'Asia/Dhaka',
 
     /*
     |--------------------------------------------------------------------------
