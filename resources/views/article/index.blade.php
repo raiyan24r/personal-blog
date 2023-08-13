e5a453b88c4ba93e1289b78e73544bf074f72e2a
raiyan24r <raiyan24r@gmail.com>
2023-08-13 17:32:26 +0600

    socials in index page

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,8 +5,9 @@
         <h1>{{ config('blog.article.title') }}</h3>
 
             <h5 style=" color:#fa6496;">{{ config('blog.article.description') }}</h5>
+            @component('particals.socials')
+            @endcomponent
         @endcomponent
-
         @include('widgets.article')
 
         {{ $articles->links('pagination.default') }}
--- resources/views/particals/socials.blade.php
+++ resources/views/particals/socials.blade.php
@@ -1,12 +1,12 @@
 <div class="font-weight-bold mt-2"> Socials : </div>
-<div class="text-center">
+<div class="text-center gap">
     <a href="https://www.instagram.com/raiyan24rih/" target="_blank" style="font-size: 24px; color: #FFFFFF; transition: color 0.3s;">
-        <i class="fab fa-instagram"></i>
+        <i class="fab fa-instagram mr-3"></i>
     </a>
     <a href="https://www.facebook.com/raiyan24rih/" target="_blank" style="font-size: 24px; color: #FFFFFF; transition: color 0.3s;">
         <i class="fab fa-facebook"></i>
     </a>
     <a href="https://www.linkedin.com/in/raiyan-ibne-hafiz/" target="_blank" style="font-size: 24px; color: #FFFFFF; transition: color 0.3s;">
-        <i class="fab fa-linkedin"></i>
+        <i class="fab fa-linkedin ml-3"></i>
     </a>
 </div>
